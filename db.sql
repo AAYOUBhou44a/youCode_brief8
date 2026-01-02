6e73629979cf92d16efd603a5a044491a729ee05
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2026-01-02 23:46:02 +0100

    la création du base données avec les tableaux nécessaires

---
+++ db.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE IF NOT EXISTS articles;
+use articles;
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY , 
+    firstName VARCHAR(100) NOT NULL , 
+    lastName VARCHAR(100) NOT NULL ,
+    email VARCHAR(150) NOT NULL UNIQUE ,
+    password VARCHAR(255) NOT NULL , 
+    role ENUM('reader','author','admin') 
+);
+
+CREATE TABLE IF NOT EXISTS articles(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    article text NOT NULL ,
+    authorId INT NOT NULL,
+    numberLikes INT NOT NULL ,
+    -- publishDate DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (authorId) REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS comments(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    comment text NOT NULL ,
+    articleId INT NOT NULL ,
+    userId INT NOT NULL ,
+    FOREIGN KEY (articleId) REFERENCES articles(id),
+    FOREIGN KEY (userId) REFERENCES users(id)
+);
